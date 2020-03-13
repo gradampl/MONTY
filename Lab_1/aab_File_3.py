b218c00233de7eff8d598fc8ffff6e17155e63b6
gradampl <gr_adam@o2.pl>
2020-03-13 19:20:31 +0100

    Create aab_File_3.py

---
+++ Lab_1/aab_File_3.py
@@ -0,0 +1,12 @@
+a = 0.24
+b = -3.14
+c = 10
+
+print('a + b + c = ', a+b+c)
+print('(a + b) * c = ', (a+b)*c)
+print('(a - b) / c = ', (a-b)/c)
+print('(a - b) // c = ', (a-b)//c)
+print('a * b * c = ', a*b*c)
+print('a - b // c = ', a-b//c)
+print('a / (b**c) = ', a/(b**c))
+print('((a%b)/(c*b))*(c-b) = ', ((a%b)/(c*b))*(c-b))
