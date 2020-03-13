8fda8d4164057551caf0b471925c4f8fe5dfec65
gradampl <gr_adam@o2.pl>
2020-03-13 16:28:28 +0100

    It's... the first file in Python

---
+++ Lab_1/HelloWorld.py
@@ -0,0 +1,29 @@
+print("Witaj świecie!")
+print()
+number1 = 12.5
+number2 = 34.3
+
+print("Działania arytmetyczne na liczbach: num1 = ", end=''
+      + str(number1)  + " i num2 = " + str(number2))
+print()
+print()
+print("Num1 + num2 = " + str(number1 + number2))
+print("Num1 - num2 = " + str(number1 - number2))
+print("Num1 / num2 = " + str(number1 / number2))   # dzielenie zwykłe
+print("Num1 // num2 = " + str(number1 // number2)) # dzielenie całkowite
+print("Num2 - num1 = " + str(number2 - number1))
+print("Num2 / num1 = " + str(number2 / number1))
+print("Num2 // num1 = " + str(number2 // number1))
+print("Num1 % num2 = " + str(number1 % number2))    # reszta z dzielenia
+print("Num2 % num1 = " + str(number2 % number1))
+print()
+print("Porównania na liczbach: num1 = ", end=''
+      + str(number1)  + " i num2 = " + str(number2))
+print()
+print()
+print("number1 > number2 = " + str(number1 > number2))
+print("number1 >= number2 = " + str(number1 >= number2) )
+print("number1 < number2 = " + str(number1 < number2))
+print("number1 <= number2 = " + str(number1 <= number2))
+print("(number1 == number2) = " + str(number1 == number2))
+
