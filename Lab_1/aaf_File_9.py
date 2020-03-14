ec095fbe27d75d8a5ddab2b974fd2d24dc142bb8
gradampl <gr_adam@o2.pl>
2020-03-14 19:59:25 +0100

    File_9: Main calls functions exclusively

--- Lab_1/aaf_File_9.py
+++ Lab_1/aaf_File_9.py
@@ -11,20 +11,40 @@ celsius = None
 def cannot_calculate():
     print('Nie ma tak niskich temperatur.')
 
-def give_degrees():
-
+def get_degrees():
     ok = False
-
     while ok == False:
         degs = input('Podaj liczbę stopni: \n')
         try:
             ok = float(degs)
-            return float(degs)
         except ValueError:
             print('Nie znam takiej liczby.\n')
-
     return float(degs)
 
+def convert_to_celsius(x):
+    if x == 'f':
+        celsius = 5 / 9 * (degrees - 32)
+    else:
+        celsius = degrees - 273.15
+    return celsius
+
+def convert_to_fahrenheit(x):
+    if x == 'c':
+        fahrenheit = 9 / 5 * degrees + 32
+    else:
+        fahrenheit = 9 / 5 * (degrees - 273.15) + 32
+    return fahrenheit
+
+def convert_to_kelvin(x):
+    if x == 'c':
+        kelvin = degrees + 273.15
+    else:
+        kelvin = 5 / 9 * (degrees - 32) + 273.15
+    return kelvin
+
+def get_scale():
+    scale = input('Podaj skalę (C = Celsjusza, F = Fahrenheita, K = Kelvina) : \n').lower()
+    return scale
 
 def scale_unknown():
     print('Nie znam takiej skali.')
@@ -45,33 +65,33 @@ def message(choice):
 
 ###########################################################################
 
-degrees = give_degrees()
-scale = input('Podaj skalę (C = Celsjusza, F = Fahrenheita, K = Kelvina) : \n').lower()
+degrees = get_degrees()
+scale = get_scale()
 
 while not (scale == 'c' or scale == 'f' or scale == 'k'):
     scale_unknown()
-    scale = input('Podaj skalę (C = Celsjusza, F = Fahrenheita, K = Kelvina): \n').lower()
+    scale = get_scale()
 
 if scale == 'c':
     while degrees < -273.15:
         cannot_calculate()
-        degrees = give_degrees()
-    fahrenheit = 9 / 5 * degrees + 32
-    kelvin = degrees + 273.15
+        degrees = get_degrees()
+    fahrenheit = convert_to_fahrenheit('c')
+    kelvin = convert_to_kelvin('c')
     message('c')
 
 elif scale == 'f':
     while degrees < -459.67:
         cannot_calculate()
-        degrees = give_degrees()
-    kelvin = 5 / 9 * (degrees - 32) + 273.15
-    celsius = 5 / 9 * (degrees - 32)
+        degrees = get_degrees()
+    celsius = convert_to_celsius('f')
+    kelvin = convert_to_kelvin('f')
     message('f')
 
 elif scale == 'k':
     while degrees < 0:
         cannot_calculate()
-        degrees = give_degrees()
-    fahrenheit = 9 / 5 * (degrees - 273.15) + 32
-    celsius = degrees - 273.15
+        degrees = get_degrees()
+    celsius = convert_to_celsius('k')
+    fahrenheit = convert_to_fahrenheit('k')
     message('k')
