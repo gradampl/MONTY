6623c27ade17dac6a6bcfe0347ec6f90e61e5f1a
gradampl <gr_adam@o2.pl>
2020-03-14 11:27:54 +0100

    Update aaf_File_9.py

--- Lab_1/aaf_File_9.py
+++ Lab_1/aaf_File_9.py
@@ -1,43 +1,68 @@
 print('Jestem konwerterem temperatury. Jeśli podasz mi temperaturę, np. w stopniach Celsjusza,')
 print('policzę, ile to jest w st. Fahrenheita i Kelvina.\n')
-degrees = float(input('Podaj liczbę stopni: \n'))
 
-fahrenheit = 0.0
-kelvin = 0.0
-celsjusz = 0.0
+degrees = None
 
-scale = input('Podaj skalę (C = Celsjusza, F = Fahrenheita, K = Kelvina: \n').lower()
-if scale == 'c':
-    fahrenheit = 9/5 * degrees + 32
-    if degrees >= -273.15:
-        kelvin = degrees + 273.15
+fahrenheit = None
+kelvin = None
+celsius = None
+ok = None
+
+################################## Teraz funkcje #######################################
+
+def cannot_calculate():
+    print('Nie ma tak niskich temperatur.')
+
+def give_degrees():
+    degs = float(input('Podaj liczbę stopni: \n'))
+    return float(degs)
+
+def scale_unknown():
+    print('Nie znam takiej skali.')
+
+def message(choice):
+    if(choice == 'c'):
+        print('Temperatura ', degrees, ' stopni Celsjusza')
+        print('to ', kelvin, ' stopni w skali Kelvina')
+        print('i ', fahrenheit, 'stopni w skali Farenheita.')
+    elif(choice == 'f'):
+        print('Temperatura ', degrees, ' stopni Farenheita')
+        print('to ', kelvin, ' stopni w skali Kelvina')
+        print('i ', celsius, 'stopni w skali Celsjusza.')
     else:
-        while degrees < -273.15:
-            print('Nie mogę przeliczyć tak niskiej temperatury na stopnie Kelvina.')
-            degrees = float(input('Podaj liczbę stopni: \n'))
-    print('Temperatura ', degrees, ' stopni Celsjusza')
-    print('to ', fahrenheit, ' stopni w skali Farenheita')
-    print('i ',kelvin, 'stopni w skali Kelvina.')
-elif scale == 'k' and degrees>=0:
-    fahrenheit = 9/5 * (degrees - 273.15) + 32
-    celsjusz = degrees - 273.15
-    print('Temperatura ', degrees, ' stopni Kelvina')
-    print('to ', fahrenheit, ' stopni w skali Farenheita')
-    print('i ', celsjusz, 'stopni w skali Celsjusza.')
-    if degrees < 0:
-        while degrees < 0:
-            print('Nie ma tak niskich temperatur.')
-            degrees = float(input('Podaj liczbę stopni: \n'))
-elif scale == 'f' and degrees >= -459.67:
-    kelvin = 5/9 * (degrees-32) + 273.15
-    celsjusz = 5/9 * (degrees-32)
-    print('Temperatura ', degrees, ' stopni Farenheita')
-    print('to ', kelvin, ' stopni w skali Kelvina')
-    print('i ', celsjusz, 'stopni w skali Celsjusza.')
-    if degrees < -459.67:
-        while degrees < 0:
-            print('Nie ma tak niskich temperatur.')
-            degrees = float(input('Podaj liczbę stopni: \n'))
+        print('Temperatura ', degrees, ' stopni Kelvina')
+        print('to ', fahrenheit, ' stopni w skali Farenheita')
+        print('i ', celsius, 'stopni w skali Celsjusza.')
 
+################################## Koniec funkcji #######################################
 
+degrees = give_degrees()
+scale = input('Podaj skalę (C = Celsjusza, F = Fahrenheita, K = Kelvina: \n').lower()
 
+while not (scale == 'c' or scale == 'f' or scale == 'k'):
+    scale_unknown()
+    scale = input('Podaj skalę (C = Celsjusza, F = Fahrenheita, K = Kelvina): \n').lower()
+
+if scale == 'c':
+    while degrees < -273.15:
+        cannot_calculate()
+        degrees = give_degrees()
+    fahrenheit = 9 / 5 * degrees + 32
+    kelvin = degrees + 273.15
+    message('c')
+
+elif scale == 'f':
+    while degrees < -459.67:
+        cannot_calculate()
+        degrees = give_degrees()
+    kelvin = 5 / 9 * (degrees - 32) + 273.15
+    celsius = 5 / 9 * (degrees - 32)
+    message('f')
+
+elif scale == 'k':
+    while degrees < 0:
+        cannot_calculate()
+        degrees = give_degrees()
+    fahrenheit = 9 / 5 * (degrees - 273.15) + 32
+    celsius = degrees - 273.15
+    message('k')
