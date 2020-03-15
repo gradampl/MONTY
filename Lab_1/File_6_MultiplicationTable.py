9da9d8814b6e3629941e60b9cc32f0e1f4b1af53
gradampl <gr_adam@o2.pl>
2020-03-15 23:41:07 +0100

    Update File_6_MultiplicationTable.py

--- Lab_1/File_6_MultiplicationTable.py
+++ Lab_1/File_6_MultiplicationTable.py
@@ -1,10 +1,15 @@
-i = 0
-
-while i < 6:
-    j = 0
-    while j < 6:
-        print(i, '*', j, '=', i * j)
-        j += 1
-        if j % 6 == 0: print('\n')
-    i += 1
-
+k = 0
+while k < 2:
+    print()
+    i = 0
+    while i < 6:
+        j = 0
+        while j < 3:
+            if j == 0:
+                print('\n', end='')
+            else:
+                print('     ',end='')
+            print(j+3*k, '*', i, '=', "%2d"%((j+3*k) * i), end='')
+            j += 1
+        i += 1
+    k += 1
