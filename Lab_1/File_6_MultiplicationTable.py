b42f1e5e57c5734c2c2d149fb5c1d5957a720f03
gradampl <gr_adam@o2.pl>
2020-03-15 23:47:59 +0100

    All columns in 1 row

--- Lab_1/File_6_MultiplicationTable.py
+++ Lab_1/File_6_MultiplicationTable.py
@@ -1,15 +1,34 @@
-k = 0
-while k < 2:
-    print()
-    i = 0
-    while i < 6:
-        j = 0
-        while j < 3:
-            if j == 0:
-                print('\n', end='')
-            else:
-                print('     ',end='')
-            print(j+3*k, '*', i, '=', "%2d"%((j+3*k) * i), end='')
-            j += 1
-        i += 1
-    k += 1
+
+i = 0
+while i < 6:
+    j = 0
+    while j < 6:
+        if j == 0:
+            print('\n', end='')
+        else:
+            print('     ',end='')
+        print(i, '*', j, '=', "%2d"%(i*j), end='')
+        j += 1
+    i += 1
+
+
+
+
+
+# dwa wiersze po trzy słupki
+
+# k = 0
+# while k < 2:
+#     print()
+#     i = 0
+#     while i < 6:
+#         j = 0
+#         while j < 3:
+#             if j == 0:
+#                 print('\n', end='')
+#             else:
+#                 print('     ',end='')
+#             print(j+3*k, '*', i, '=', "%2d"%((j+3*k) * i), end='')
+#             j += 1
+#         i += 1
+#     k += 1
