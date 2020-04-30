8b5ca2fe30acecf14adf135455e1ed139023b54c
gradampl <gr_adam@o2.pl>
2020-04-30 21:36:55 +0200

    Create daf.py

---
+++ Lab_4/daf.py
@@ -0,0 +1,26 @@
+def sort_check(list):
+    for i in range(0, len(list) + 1):
+        if list[i] <= list[i + 1]:
+            return False
+        else:
+            return True
+
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [5, 5, 5, 5, 5]
+list3 = [5, 5, 4, 4, 3]
+list4 = [5, 4, 3, 2, 1]
+
+print(sort_check(list1))
+print(sort_check(list2))
+print(sort_check(list3))
+print(sort_check(list4))
+
+# Result:
+#
+# False
+# False
+# False
+# True
+#
+# Process finished with exit code 0
